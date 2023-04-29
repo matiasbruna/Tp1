71d3948b09099a784bb9cce2bac3a2ba586f27b8
Javier Rassetto <javi.rassetto@gmail.com>
2023-04-29 16:49:06 -0300

    "Modificaciones en boton seguir"

--- src/script/btnSeguir.js
+++ src/script/btnSeguir.js
@@ -4,15 +4,20 @@ document.getElementById("btnSeguir").onclick= function(){
     Seguir();
 }
 
+/* funcion Seguir cambio de info y cambio de colores*/
 function Seguir(){   
    if(document.getElementById("btnSeguir").innerHTML=="Seguir") {
    document.getElementById("btnSeguir").innerHTML="Dejar de Seguir";
-   document.getElementById("btnSeguir").style.color="black";
+   document.getElementById("btnSeguir").style.background="darkblue";
+   document.getElementById("btnSeguir").style.color="white";
     }
     else{
         document.getElementById("btnSeguir").innerHTML="Seguir";
         document.getElementById("btnSeguir").style.color="white";
+        document.getElementById("btnSeguir").style.background="rgb(59,130, 246)";
+       
     }  
+
 }
 
 /*Evento click caja cambio de color */
