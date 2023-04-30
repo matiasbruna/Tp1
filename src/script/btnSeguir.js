deed642ebc66f8a25a2328c84cf804da31f0dcaa
Matias <matias.b.dev@gmail.com>
2023-04-30 12:09:02 -0300

    guardo cambios

--- src/script/btnSeguir.js
+++ src/script/btnSeguir.js
@@ -7,7 +7,7 @@ document.getElementById("btnSeguir").onclick= function(){
 function Seguir(){   
    if(document.getElementById("btnSeguir").innerHTML=="Seguir") {
    document.getElementById("btnSeguir").innerHTML="Dejar de Seguir";
-   document.getElementById("btnSeguir").style.color="black";
+   document.getElementById("btnSeguir").style.backgroundColor="black";
     }
     else{
         document.getElementById("btnSeguir").innerHTML="Seguir";
@@ -25,7 +25,7 @@ color.addEventListener("click",CambiarColor,true)
 function CambiarColor(){
    if(contador==0){
 
-
+    color.style.innerHTML
     caja.classList.add=('rojo');
     contador=-1;
 
